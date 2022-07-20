700223b34a97322763ec1f4c6d7cd2c18ce78134
Geri-86 <muhamadgeriramdan@smkwikrama.sch.id>
2022-07-20 17:04:38 +0700

    Change style for dekstop and mobile

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -9,7 +9,7 @@ const Card = ({ pokemon, loading,infoPokemon}) => {
                     return (
                         <>
                             <div className="card" key={item.id} onClick={()=>infoPokemon(item)}>
-                                <h2>{item.id}</h2>
+                                <h2 className="pokemon-number">{item.id}</h2>
                                 <img src={item.sprites.front_default} alt="" />
                                 <h2>{item.name}</h2>
                             </div>
